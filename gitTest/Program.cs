3487dd551921423963ad372e6ad47446571bbdaf
Wesley Ax <contact@wesleyax.com>
2021-06-10 14:48:10 -0300

    kndkjvnfd

--- gitTest/Program.cs
+++ gitTest/Program.cs
@@ -4,6 +4,8 @@ namespace gitTest {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
