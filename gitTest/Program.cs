992cc3e583c1feaa67b90d718e73fcc2d057a461
Wesley Ax <contact@wesleyax.com>
2021-06-10 16:07:16 -0300

    Search a day

--- gitTest/Program.cs
+++ gitTest/Program.cs
@@ -3,12 +3,41 @@
 namespace gitTest {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            int number = 1;
+            string day = "";
+
+            while (number > 0 && number < 8) {
+                Console.Write("Write a number from 1 to 7: ");
+                number = int.Parse(Console.ReadLine());
+
+                switch (number) {
+                    case 1:
+                        day = "Sanday";
+                        break;
+                    case 2:
+                        day = "Mondey";
+                        break;
+                    case 3:
+                        day = "Tuesday";
+                        break;
+                    case 4:
+                        day = "Wednesday";
+                        break;
+                    case 5:
+                        day = "Thursday";
+                        break;
+                    case 6:
+                        day = "Friday";
+                        break;
+                    case 7:
+                        day = "Saturday";
+                        break;
+                    default:
+                        day = "Invalid value";
+                        break;
+                }
+                Console.WriteLine("Day: " + day);
+            }
         }
     }
 }
